7a634aee52eacedaddae96f44516837bf549df5b
Flaeriel <77295870+Flaeriel@users.noreply.github.com>
2021-01-12 05:22:10 +0100

    Allowing Multiple Dice Rolls

--- commands/roll.js
+++ commands/roll.js
@@ -9,29 +9,28 @@ module.exports = {
     description: 'rolling dice',
     args: true,
     execute (message, args) {
-        let call = args_handler.diceMatch(args[0]);
-        let splits = args_handler.diceSplit(args[0], call.pattern);
-        //console.log(call);
+        let collect = [];
+        for (let i = 0; i < args.length; i++) {
+            let call = args_handler.diceMatch(args[i]);
+            let splits = args_handler.diceSplit(args[i], call.pattern);
 
-        if (args.length > 1) {
-            response.ignore(message, args);
-        }
+            if (call.name === 'calc') {
+                throw "Calc Not Implemented Error!";
 
-        if (call.name === 'calc') {
-            throw "Calc Not Implemented Error!";
-
-        } else if (call.name === 'dice' || call.name === 'dice_mod' ) {
-            let dice;
-            if (call.name === 'dice_mod') {
-                dice = new DiceModClass(splits[1], splits[2], splits[3], splits[4]);
-            } else if (call.name === 'dice') {
-                dice = new DiceClass(splits[1], splits[2]);
+            } else if (call.name === 'dice' || call.name === 'dice_mod' ) {
+                let dice;
+                if (call.name === 'dice_mod') {
+                    dice = new DiceModClass(splits[1], splits[2], splits[3], splits[4]);
+                } else if (call.name === 'dice') {
+                    dice = new DiceClass(splits[1], splits[2]);
+                }
+                let total = dice.add_up(dice.roll());
+                // logging the dice object
+                console.log(dice);
+                collect.push(response.dice_result(message, total, dice));
             }
-            let total = dice.add_up(dice.roll());
-            // logging the dice object
-            console.log(dice);
-            return response.dice_result(message, total, dice);
         }
+        return collect;
     },
 
 };
